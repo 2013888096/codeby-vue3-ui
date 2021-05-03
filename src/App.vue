b66019512aa2c06d4738a8cc06e6713edf362b54
2013888096 <2013888096@qq.com>
2021-05-03 11:26:16 +0800

    初始化提交代码

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <template>
-  <div>hi</div>
-  <frank />
+  <router-view />
 </template>
 
 <script>
-import Frank from './components/Frank.vue'
-
 export default {
   name: 'App',
-  components: {
-    Frank
-  }
 }
 </script>
