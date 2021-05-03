a5bc87dbe923e860f8c17c7891d8a6b0203c359a
2013888096 <2013888096@qq.com>
2021-05-03 14:26:55 +0800

    完善aside切换逻辑

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,10 @@ import { ref, provide } from "vue";
 export default {
   name: "App",
   setup() {
-    const menuVisible = ref(false);
+    //   通过获取页面宽度来决定  menuVisible 初始化为true 还是false
+    const width = document.documentElement.clientWidth;
+    const menuVisible = ref(width <= 500 ? false : true);
+
     provide("menuVisible", menuVisible); // set
   },
 };
--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="top_nav">
-    <div class="logo"
-         @click="toggleAside">LOGO</div>
+    <div class="logo">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
-    <span class="toggleAside"></span>
+    <span class="toggleAside"
+          @click="toggleAside">
+      <svg class="icon">
+        <use xlink:href="#icon-menu"></use>
+      </svg>
+    </span>
   </div>
 
 </template>
