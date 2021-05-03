d82fe780c741a1e9bb929854b5c34c6169f169c4
2013888096 <2013888096@qq.com>
2021-05-03 20:40:16 +0800

    可以接受value和input事件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
-    Switch Demo的文档
+    <Switch v-model:value="checked" />
   </div>
 </template>
+<script lang="ts">
+import Switch from "../lib/Switch.vue";
+import { ref } from "vue";
+export default {
+  components: { Switch },
+  setup() {
+    const checked = ref(false);
+    return {
+      checked,
+    };
+  },
+};
+</script>
 <style lang="scss" scoped>
 </style>
