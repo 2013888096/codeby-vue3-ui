d403044a9a101643346e37486367ac2fd0ded9c9
2013888096 <2013888096@qq.com>
2021-05-13 09:08:24 +0800

    支持markdown

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,10 @@ import ButtonDemo from '../codeDemo/ButtonDemo.vue'
 import DialogDemo from '../codeDemo/DialogDemo.vue'
 import TabsDemo from '../codeDemo/TabsDemo.vue'
 import Expect from '../codeDemo/Expect.vue'
-import Intro from '../docDemo/Intro.vue'
+import Intro from '../views/Intro.vue'
+import docdemo from '../docDemo/docdemo.vue'
+import Install from '../views/Install.vue'
+import GetStarted from '../views/GetStarted.vue'
 
 const history = createWebHashHistory();
 export const router = createRouter({
@@ -23,6 +26,19 @@ export const router = createRouter({
             children: [
                 {
                     path: '',
+                    component: docdemo
+                }
+                ,
+                {
+                    path: 'get-started',
+                    component: GetStarted
+                },
+                {
+                    path: 'install',
+                    component: Install
+                },
+                {
+                    path: 'intro',
                     component: Intro
                 },
                 {
